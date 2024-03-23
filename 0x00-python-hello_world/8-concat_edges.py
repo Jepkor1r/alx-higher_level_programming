39cd4c4b5a461d27c6ea7fcfb8fc58cbbaed77b1
Jepkor1r <jlagat007@gmail.com>
2024-03-23 11:48:14 -0700

    Python - Hello, World

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str.find('object-oriented programming with Python')
+str = str(38:53) + str(55:65) + str(103:106) + str(0:5)
 print(str)

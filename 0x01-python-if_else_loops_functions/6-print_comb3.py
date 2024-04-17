3bb4f38a903595206e1cf082f2dd49b8435ef189
Jepkor1r <jlagat007@gmail.com>
2024-04-16 23:49:08 -0700

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 for i in range(9):
-    print("{}".format(i), end='')
-    for j in range(10):
-        print("{}, ".format(j), end='')
+    for j in range(i + 1,10):
+        if i == j:
+            continue
+        if i < 8:
+            print("{}".format(i), end='')
+            print("{}, ".format(j), end='')
+        else:
+            print("{}{}{}".format(i, j, "\n"))
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    """
+       uppercase() - prints a string in uppercase
+       str - parameter passed
+    """
+    for i in range(ord('A'), ord('Z')):
+        print("{}".format(i))
+        print("{}".format("\n"))

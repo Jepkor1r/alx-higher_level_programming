a08c4ec74299bb8afc4d420c05ab7b1233caf8e0
Jepkor1r <jlagat007@gmail.com>
2024-04-17 11:16:19 -0700

    Python-Import Modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -6,5 +6,4 @@ b = 2
 print("{} + {} = {}".format(a, b, add(a,b)))
 
 if __name__ != "__main__":
-    raise ImportError
-    ("This module cannot be directly imported")
+    raise ImportError("This module cannot be directly imported")

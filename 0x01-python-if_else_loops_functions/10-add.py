f685e901dd13ec76fdb0f2f0279499f0010b256a
Jepkor1r <jlagat007@gmail.com>
2024-04-18 09:25:54 -0700

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -3,8 +3,8 @@
 def add(a, b):
     """
         add - adds two integers
-	a - integer 1
-	b - integer 2
-	Return - result of addition
+        a - integer 1
+        b - integer 2
+        Return - result of addition
     """
     return a + b

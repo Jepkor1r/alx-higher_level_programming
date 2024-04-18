3d91090123aa43c09f59e192f137d0302a284bb6
Jepkor1r <jlagat007@gmail.com>
2024-04-18 09:21:45 -0700

    Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """
+        add - adds two integers
+	a - parameter integer 1
+	b - paremeter integer 2
+	Return - result of addition
+    """
+    return a + b

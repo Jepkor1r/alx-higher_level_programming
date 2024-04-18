fe5d842369d83e178f88963593f66f9a6842b618
Jepkor1r <jlagat007@gmail.com>
2024-04-18 10:02:41 -0700

    Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    """
+        print_last_digit - prints the last digit of number
+        number - parameter value to be checked
+	Return - value of last digit
+    """
+    return number % 10

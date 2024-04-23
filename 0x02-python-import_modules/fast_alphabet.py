e2e24b8a4e9a57440156a3eb18d69a26f3db7ade
Jepkor1r <jlagat007@gmail.com>
2024-04-23 08:41:50 -0700

    Python-Import Modules

--- 0x02-python-import_modules/fast_alphabet.py
+++ 0x02-python-import_modules/fast_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(65, 91):
-    print("{}".format(chr(i)), end="" if i <= 90 else "{}".format("\n"))
+    print("{}".format(chr(i)), end="" if i < 90 else "{}{}".format(i, "\n"))

323cf2c85d8bc78097522f8a45fcec08cec5bb6d
Jepkor1r <jlagat007@gmail.com>
2024-05-14 02:29:47 -0700

    Data Structures: Lists, Tuples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@
 def multiple_returns(sentence):
 	length = len(sentence)
 	first = sentence[0]
-	if sentence == "":
+	if sentence == sentence():
 	    first = "None"
 	    return length, first
 	else:

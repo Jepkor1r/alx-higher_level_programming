d7d0d1266466becbb8f3f236192e064675685780
Jepkor1r <jlagat007@gmail.com>
2024-05-15 02:28:42 -0700

    classes and objects

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Class Square: empty class
+init: stands for initialization method
+self: instance attribute
+__size: private instance attribute
+"""
+
+
+class Square:
+    """
+        Square: an empty class
+        It defines a class
+    """
+    def __init__(self, __size):
+        """
+            init: used to initialize a instance
+            self: self initializing instance attribute
+            __size: private instance attribute
+        """
+        self.__size = __size
+
+    try:
+        def __init__(self, size=0):
+            """
+                init: used to initialize an instance
+                self: self initializing instance attribute
+                size: instance attribute
+            """
+    except TypeError:
+        print("size must be an integer")
+    except ValueError:
+        print("size must be >= 0")

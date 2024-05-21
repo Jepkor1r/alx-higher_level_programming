e3a5b1366a8d0914b2633e7908937e115edb9e54
Jepkor1r <jlagat007@gmail.com>
2024-05-21 16:59:14 -0700

    sql basics

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 -- Lists the number of records with the same score in the table
-SELECT score, COUNT(DISTINCT score) AS number
+SELECT score, COUNT(*) AS number
 FROM second_table
+GROUP BY score
 ORDER BY score DESC;

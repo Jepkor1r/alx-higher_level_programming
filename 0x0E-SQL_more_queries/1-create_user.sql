a4ee8613dbc6e7c1e2e72ecc7d84b73c1c846b3e
Jepkor1r <jlagat007@gmail.com>
2024-05-22 14:35:21 -0700

    sql-more queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 -- should have all priviledge
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT ALL ON hbtn_0d_2.* TO 'user_0d_1'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_1'@'localhost';

7e61032f93290b4f7d949c8c51e6ac5cae64251a
Jepkor1r <jlagat007@gmail.com>
2024-05-24 23:29:17 -0700

    classes and objects

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,9 +5,7 @@ init: stands for initialization method
 self: instance attribute
 __size: private instance attribute
 """
-
-
-class Square:
+class Square():
     """
         Square: an empty class
         It defines a class
@@ -23,3 +21,11 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+            area: used to calculate the area of squared size
+	    return : area of a squared size
+        """
+        return self.__size **2
+        return area(self.__size **2)

e8ac3c68fcdaacd3416c5e2da44f89c0f52b1b3d
Jepkor1r <jlagat007@gmail.com>
2024-05-25 17:43:29 -0700

    classes and objects

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+Class Square: empty class
+init: stands for initialization method
+self: instance attribute
+__size: private instance attribute
+"""
+
+
+class Square():
+    """
+        Square: an empty class
+        It defines a class
+    """
+    def __init__(self, size=0):
+        """
+            init: used to initialize an instance
+            self: self initializing instance attribute
+            size: instance attribute
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+            size: method used for geting data is decorated with '@property'
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+            setter: for changing data
+            @size: decorates setter with method name of the property decorator
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+            area: used to calculate the area of squared size
+            return : area of a squared size
+        """
+        return self.__size ** 2
+        return area(self.__size ** 2)
+
+    def my_print(self):
+        """
+            my_print: prints in stdout the square with the character #
+        """
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("{}".format("#"), end="")
+                print("")

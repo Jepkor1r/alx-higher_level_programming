167af8be238cc2817203bab09f2c8c7b4495d54d
Jepkor1r <jlagat007@gmail.com>
2024-06-15 05:11:29 -0700

    Python-Import Modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 import sys
-if len(sys.argv) == 1:
-    print("0 arguments.")
-elif len(sys.argv) == 2:
-    print("1 argument:\n1: {}".format(sys.argv[1]))
-else:
-    print("{} arguments:\n{}".format(len(sys.argv) - 1, "\n".join(["{}: {}".format(i, sys.argv[i]) for i in range(1, len(sys.argv))])))
-
-if __name__ = "__main__":
-    raise ImportError("This module cannot be directly imported")
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:\n1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:\n{}".format(len(sys.argv) - 1, "\n".join(["{}: {}".format(i, sys.argv[i]) for i in range(1, len(sys.argv))])))

8c0201ce603e23f80886444a9e55680249c74395
Jepkor1r <jlagat007@gmail.com>
2024-06-16 04:38:43 -0700

    Python-Import Modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,2 +1,9 @@
-#!/isr/bin/python3
-
+#!/usr/bin/python3
+import hidden_4
+if __name__ == "__main__":
+    #dir function - list all attributes in hidden_4 module alphabetically
+    for i in dir(hidden_4):
+        #skips all attributes starting with "__"
+        if i[:2] == "__":
+            continue
+        print("{:s}".format(i))

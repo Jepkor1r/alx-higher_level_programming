2b52e7a20870805e1a9cd4daa887ad1625cb2f6e
Jepkor1r <jlagat007@gmail.com>
2024-06-17 01:06:44 -0700

    Data Structures: Lists, Tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,9 +2,17 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     result = (0, 0)
-    result = (result[0] + tuple_a[0] + tuple_b[0], result[1])
+
+    # Add the first element of each tuple if it exists
+    if len(tuple_a) > 0:
+        result = (result[0] + tuple_a[0], result[1])
+    if len(tuple_b) > 0:
+        result = (result[0] + tuple_b[0], result[1])
+
+    # Add the second element of each tuple if it exists
     if len(tuple_a) > 1:
         result = (result[0], result[1] + tuple_a[1])
     if len(tuple_b) > 1:
         result = (result[0], result[1] + tuple_b[1])
+
     return result

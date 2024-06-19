70085d8c3e5506ac56050f7909b7096aff5a9b02
Jepkor1r <jlagat007@gmail.com>
2024-06-19 00:15:47 -0700

    JavaScript - Warm up

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let argsNum = process.argv;
-let sentence = argsNum[2] + ' is ' + argsNum[3];
+const argsNum = process.argv;
+const sentence = argsNum[2] + ' is ' + argsNum[3];
 
 console.log(sentence);

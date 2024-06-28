93cffeb7407338a47ff41f31f48daac2e191dbca
Jepkor1r <jlagat007@gmail.com>
2024-06-28 03:53:47 -0700

    More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,10 +2,8 @@
 
 def complex_delete(a_dictionary, value):
     # Create a list to store keys that need to be deleted
-    keys_to_delete = [key for key in a_dictionary if a_dictionary[key] == value]
-    
+    delete_keys = [key for key in a_dictionary if a_dictionary[key] == value]
     # Delete the keys from the dictionary
-    for key in keys_to_delete:
+    for key in delete_keys:
         del a_dictionary[key]
-    
     return a_dictionary

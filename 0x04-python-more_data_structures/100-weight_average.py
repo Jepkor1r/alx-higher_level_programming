3072e2184a4263bbbaabcbdf28b813dbc15c3e21
Jepkor1r <jlagat007@gmail.com>
2024-06-27 19:06:48 -0700

    More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    score = 0
+    weight = 0
+    for row in my_list:
+        mul = row[0] * row[1]
+        score += mul
+        weight += row[1]
+        average = score / weight
+    return average

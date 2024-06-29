1069c09204ab76ec256895ed7721cd44ffebd667
Jepkor1r <jlagat007@gmail.com>
2024-06-29 09:02:45 -0700

    Python - Exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value), end="\n")
         return True
     except (ValueError, TypeError) as e:
+        # sys.stderr: redirects the error msg to standard error output stream
+        # file was used to explicitly print stderr. Why?
+        # by default print has  file parameter where output is to  be directed
         print(f"Exception: {e}", file=sys.stderr)
         return False

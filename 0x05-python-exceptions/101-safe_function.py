d373248997faa6567a17808d922d6aaf56103f40
Jepkor1r <jlagat007@gmail.com>
2024-06-29 09:49:53 -0700

    Python - Exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,6 +4,12 @@ import sys
 
 def safe_function(fct, *args):
     try:
+        # fct: function name to be called
+        # *args: any number of arguments is accepted
+        # for example: add(1, 2, 3)
+        # add is the function while (1, 2, 3) are arguments
+        # So *args is like?
+        # having a box to hold any number of balls yet you dont know how many
         return fct(*args)
     except Exception as e:
         print(f"Exception: {e}", file=sys.stderr)

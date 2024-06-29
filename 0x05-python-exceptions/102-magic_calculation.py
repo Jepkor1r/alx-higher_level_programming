92df47c12fbe531bae9ae4dc70139f74ca45e4b8
Jepkor1r <jlagat007@gmail.com>
2024-06-29 13:27:42 -0700

    Python - Exceptions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,7 +4,7 @@ def magic_calculation(a, b):
     try:
         result = 0
         for i in range(1, 3):
-            if a > i:
+            if i > a:
                 raise Exception("Too far")
             else:
                 result += (a ** b) / float(i)

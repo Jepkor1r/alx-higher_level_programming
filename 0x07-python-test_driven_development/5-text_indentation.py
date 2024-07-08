64cd0d90450a3e2815cc3fd506da63d9aeb042e5
Jepkor1r <jlagat007@gmail.com>
2024-07-08 03:07:48 -0700

    TDD

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,39 +1,37 @@
 #!/usr/bin/python3
+
 """
-    This module prints a text with 2 new lines,
-    after each of these characters: ., ? and :
-    text_indentation: this is the function
-    Args: text, must be a string
-    Print: a text with 2 new lines
-    after each of these characters: ., ? and :
-    Also, no space at the beginning or at the end of each printed line
-    Raises: TypeError, if text is not a string
+Text Indentation module
+This module adds two new lines after these characters: ".?:"
+and doesn't print any spaces at the beginning or end
+of the given sentence.
+Prototype: def text_indentation(text):
+Raise exceptions if text is not a string.
 """
 
 
 def text_indentation(text):
     """
-        It prints a text with 2 new lines,
-        after each of these characters: ., ? and :
-        with no space at the beginning or at the end
-        Args:
-            text, must be a string
-        Print: text
-        Raises:
-         TypeError: text must be a string
+    Function that prints 2 new lines after these chars:
+    ".", "?" and ":"
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    result = " "
-    erase_spaces = True
+    # initialize variable to hold final string
+    final_str = ""
 
+    # flag to skip/trim leading whitespaces
+    trim_spaces = True
+
+    # iterate through all chars in the given text
     for char in text:
+        # check if char is one of these: '.', '?' or ':'
         if char in ".?:":
-            result += char + "\n\n"
-            erase_spaces = True
-        else:
-            if char != " " or not erase_spaces:
-                result += char
-            erase_spaces = False
-    print(result.lstrip())
+            final_str += char + "\n\n"  # add char and two new lines
+            trim_spaces = True  # set flag to skip leading whitespaces
+        elif char != " " or not trim_spaces:
+            final_str += char
+            trim_spaces = False  # reset flag if it's a non-space character
+
+    print(final_str)

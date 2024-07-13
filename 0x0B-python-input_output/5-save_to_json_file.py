c9cbdd16ddd9704fb9a7576ddfad3367f777a2aa
Jepkor1r <jlagat007@gmail.com>
2024-07-12 23:55:16 -0700

    Python - Input/Output

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -14,7 +14,6 @@ def save_to_json_file(my_obj, filename):
             my_obj: object to be decoded
             filename: name of the file
     """
-    my_string = json.dumps(my_obj)
     with open(filename, "w", encoding="UTF-8") as f:
-        content = f.write(my_string)
-    return content
+        json.dump(my_obj, f)
+    return filename

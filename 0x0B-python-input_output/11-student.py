0deb472ac6a94ea00a16a539168cf17fbb9afa71
Jepkor1r <jlagat007@gmail.com>
2024-07-13 05:57:32 -0700

    Python - Input/Output

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+
+    In this module, class Student defines a student
+
+"""
+
+
+class Student:
+    """
+        class Student that defines a student by:
+        Public instance attributes:
+            first_name
+            last_name
+            age
+        Instantiation with first_name, last_name and age
+        Public method(to_json) retrieves dictionary repr. of a Student instance
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+            Initializes the student instance with first_name,last_name,age
+
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+            public method that retrieves a dict. repr. of Student instance
+            If attrs is a list of strings,
+            only attribute names contained in this list are retrieved
+        """
+        if attrs is None:
+            return self.__dict__
+        if not isinstance(attrs, list):
+            return self.__dict__
+        if not (isinstance(attrs[elements], str) for elements in attrs):
+            return self.__dict__
+        return {key: value for key, value in self.__dict__.items()
+                if key in attrs}
+
+    def reload_from_json(self, json):
+        """
+            This method takes a dictionary json and updates:
+            the instance's attributes with the key-value pairs from the dict.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)

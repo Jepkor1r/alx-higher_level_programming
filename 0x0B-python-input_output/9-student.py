29f7d03788a6e787f9947316c72c2b840d5a2045
Jepkor1r <jlagat007@gmail.com>
2024-07-13 04:39:28 -0700

    Python - Input/Output

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+"""
+
+    In this module, class Student defines a student
+
+"""
+
+
+class Student:
+    """
+        class Student that defines a student by:
+        Public instance attributes:
+            first_name
+            last_name
+            age
+        Instantiation with first_name, last_name and age
+        Public method(to_json) retrieves dictionary repr. of a Student instance
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+            Initializes the student instance with first_name,last_name,age
+
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+            public method that retrieves a dict. repr. of Student instance
+
+        """
+        return self.__dict__

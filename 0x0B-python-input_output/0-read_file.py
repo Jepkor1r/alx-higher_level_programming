84b18c87a10dfa04e05604bceb273345c8a8fb02
Jepkor1r <jlagat007@gmail.com>
2024-07-12 21:58:10 -0700

    Python - Input/Output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+
+    This module reads a text file and prints it to stdout
+
+"""
+def read_file(filename=""):
+    """
+        Function that reads a text file and prints it to stdout
+        Arguments:
+            filename, UTF encoded
+        with: used to close a file even if an exception is raised at some point
+        Prints: the filename
+    """
+    with open(filename, encoding="UTF-8") as f:
+        content  = f.read()
+    print(content, end='')

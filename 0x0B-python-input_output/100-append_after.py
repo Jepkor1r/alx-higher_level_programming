59860ed29435618149336c62fd5fb4ca9ec077e3
Jepkor1r <jlagat007@gmail.com>
2024-07-13 06:37:15 -0700

    Python - Input/Output

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -8,5 +8,14 @@ def append_after(filename="", search_string="", new_string=""):
     """
         A function that takes a file , search for a string and write
     """
-    with open(filename, "r+", encoding="utf-8") as f:
-        data = f.read()
+    with open(filename, 'r', encoding='utf-8') as f:
+        line_list = []
+        while True:
+            line = f.readline()
+            if line == "":
+                break
+            line_list.append(line)
+            if search_string in line:
+                line_list.append(new_string)
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.writelines(line_list)
